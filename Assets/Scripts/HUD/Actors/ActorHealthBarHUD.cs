9246faac31518e0eb582e8d2e4f3ce12fd01919a
whoadrian <whoa.adrian@gmail.com>
2023-05-06 11:07:34 +0200

    actor HUDs

---
+++ Assets/Scripts/HUD/Actors/ActorHealthBarHUD.cs
@@ -0,0 +1,52 @@
+using System;
+using Chimera.Combat;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace Chimera
+{
+    public class ActorHealthBarHUD : MonoBehaviour
+    {
+        public RectTransform parent;
+        public Image healthAmountImg;
+
+        private Actor _actor;
+
+        private void Awake()
+        {
+            _actor = transform.parent.GetComponent<Actor>();
+        }
+
+        private void Start()
+        {
+            Color color;
+            switch (_actor.Faction)
+            {
+                case Faction.Red: 
+                    color = Color.red;
+                    break;
+                case Faction.Green: 
+                    color = Color.green;
+                    break;
+                case Faction.Blue: 
+                    color = Color.blue;
+                    break;
+                default:
+                    throw new NotImplementedException();
+            }
+
+            healthAmountImg.color = color;
+        }
+
+        private void Update()
+        {
+            if (Camera.main?.transform)
+            {
+                parent.LookAt(Camera.main.transform);
+            }
+
+            var healthPercentage = _actor.CurrentHealth / _actor.MaxHealth;
+            healthAmountImg.fillAmount = healthPercentage;
+        }
+    }
+}
---
+++ Assets/Scripts/HUD/Actors/ActorSelectableHUD.cs
@@ -0,0 +1,41 @@
+using System;
+using Chimera.Combat;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace Chimera
+{
+    public class ActorSelectableHUD : MonoBehaviour
+    {
+        public Image selectable;
+
+        private Actor _actor;
+
+        private void Awake()
+        {
+            _actor = transform.parent.GetComponent<Actor>();
+        }
+        
+        private void Start()
+        {
+            Color color;
+            switch (_actor.Faction)
+            {
+                case Faction.Red: 
+                    color = Color.red;
+                    break;
+                case Faction.Green: 
+                    color = Color.green;
+                    break;
+                case Faction.Blue: 
+                    color = Color.blue;
+                    break;
+                default:
+                    throw new NotImplementedException();
+            }
+
+            selectable.color = color;
+            selectable.enabled = false;
+        }
+    }
+}
