24e9f6c9ee136a9fc1898059d2e93aba81039ae6
whoadrian <whoa.adrian@gmail.com>
2023-05-07 10:29:33 +0200

    player faction and camera config cleanup

--- Assets/Scripts/Camera/CameraConfig.cs
+++ Assets/Scripts/Camera/CameraConfig.cs
@@ -10,14 +10,13 @@ namespace Chimera
         public float moveSmoothing = 10;
 
         [Header("Rotation")] 
-        public float cameraAngle = 45;
         public float rotationSpeed = 10;
         public float rotationSmoothing = 10;
         
         [Header("Zoom")]
         public float zoomSpeed = 10;
+        public float zoomSmoothing = 10;
         public float minZoomSize = 10;
         public float maxZoomSize = 100;
-        public float zoomSmoothing = 10;
     }
 }
